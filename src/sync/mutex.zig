c79b543fdb51d3afd861290f62d734946b61a60b
Pavel Skripkin <paskripkin@gmail.com>
2026-06-20 16:00:22 +0300

    add missing module

---
+++ src/sync/mutex.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const Thread = std.Thread;
+const Value = std.atomic.Value;
+
+// FIXME: assume linux semantics where TID != 0.
+const InvalidID: Thread.Id = 0;
+const AtomicID = Value(Thread.Id);
+
+// Mutex with perks
+pub const Mutex = struct {
+    mtx: std.Io.Mutex,
+    owner: AtomicID,
+
+    pub const init: Mutex = .{ .mtx = std.Io.Mutex.init, .owner = AtomicID.init(InvalidID) };
+
+    pub fn lockUncancelable(self: *Mutex, io: std.Io) void {
+        const tid = Thread.getCurrentId();
+
+        if (self.owner.load(.monotonic) == tid) {
+            @panic("Mutex deadlock");
+        }
+
+        self.mtx.lockUncancelable(io);
+        const old_owner = self.owner.swap(Thread.getCurrentId(), .monotonic);
+        std.debug.assert(old_owner == InvalidID);
+    }
+
+    pub fn assert_locked(self: *Mutex) void {
+        std.debug.assert(self.owner.load(.monotonic) == Thread.getCurrentId());
+    }
+
+    pub fn assert_not_locked(self: *Mutex) void {
+        std.debug.assert(self.owner.load(.monotonic) != Thread.getCurrentId());
+    }
+
+    pub fn unlock(self: *Mutex, io: std.Io) void {
+        const tid = Thread.getCurrentId();
+        const old_owner = self.owner.swap(InvalidID, .monotonic);
+
+        std.debug.assert(tid == old_owner);
+        self.mtx.unlock(io);
+    }
+};
---
+++ src/sync/root.zig
@@ -0,0 +1,1 @@
+pub const mutex = @import("mutex.zig");
